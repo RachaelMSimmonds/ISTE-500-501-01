d3e1df282bcf31ccb68804ffb9cfa4cd3942ff36
nathankaufman <natecat4444@gmail.com>
2021-03-07 16:01:15 -0500

    Admin and usernav stubs

---
+++ arts/src/navigation/adminNav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Link } from "react-router-dom";
+import "antd/dist/antd.css";
+import "./../index.css";
+
+function AdminNav(){
+	return(<nav></nav>);
+}
+
+export default AdminNav;
---
+++ arts/src/navigation/userNav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Link } from "react-router-dom";
+import "antd/dist/antd.css";
+import "./../index.css";
+
+function UserNav(){
+	return(<nav></nav>);
+}
+
+export default UserNav;
