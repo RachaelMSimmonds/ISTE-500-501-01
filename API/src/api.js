a4b1e079354f19845fc08a618e76630f89adc4b2
jold01 <jack.old@outlook.com>
2021-03-28 03:44:27 -0400

    Created new file structure to better organize API. Created QueryBuilder module to dedicated to querying the database.

--- API/src/api.js
+++ API/src/api.js
@@ -2,43 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mysql = require("mysql");
 
+const user = require("./routes/user");
+const pass = require("./routes/pass");
+
+
 const app = express();
 const port = 5000;
 dotenv.config();
 
-const conn = mysql.createConnection({
-    host     : process.env.DB_HOST,
-    user     : process.env.DB_USER,
-    password : process.env.DB_PASS,
-    database : process.env.DB_NAME
-});
-
-//////////////////////TEST CONNECTION//////////////////////////
-
-// conn.connect((err) => {
-//     if (err) {
-//       console.error('error connecting: ' + err.stack);
-//       return;
-//     }
-   
-//     console.log('DB CONNECTION SUCCESSFUL: connected as id ' + conn.threadId);
-//     conn.end()
-// })
-//conn.end()
-
-
-
-app.get('/', (req, res) => {
-    res.send('hello world')
-})
-
-app.get('/db', (req, res) => {
-    conn.query('DESC `users`', (error, results, fields) => {
-        if (error) throw error;
-        res.send(results)
-    })
-
-})
+app.use("/user", user);
+app.use("/pass", pass);
 
 
 
