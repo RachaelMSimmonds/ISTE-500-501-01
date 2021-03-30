ad310535fbb5ae272cb50b5ad100de711f80c2c2
nathankaufman <natecat4444@gmail.com>
2021-03-30 13:43:06 -0400

    Passapp1

--- arts/src/user/confirmEmail.jsx
+++ arts/src/user/confirmEmail.jsx
@@ -11,18 +11,45 @@ import {
   Button,
   AutoComplete,
 } from 'antd';
+import { useParams } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import "antd/dist/antd.css";
 import "./../index.css";
 
 //wireframes 11 &12
 function ConfirmEmail(){
+	
+	let params = useParams();
+	let id = params.id;
 	const [form] = Form.useForm();
 	
 	const onFinish = (values) => {
     	console.log('Received values of form: ', values);
+		<Redirect to="/userportal/1" />
   	};
+	
+	const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
+	};
+	
 	return(<div>
 			<Form 
+				{...formItemLayout}
 				form ={form}
 				name="confirmemail"
 				onFinish={onFinish}
@@ -59,6 +86,12 @@ function ConfirmEmail(){
       				>
         				<Input />
       			</Form.Item>
+				
+				<Form.Item>
+					<Button type='primary' htmlType='submit' href='/userportal/1'>
+						Submit
+					</Button>
+				</Form.Item>
 			</Form>
 		</div>);
 }
--- arts/src/user/passApp2.jsx
+++ arts/src/user/passApp2.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+  Form,
+  Input,
+  Tooltip,
+  Select,
+  Row,
+  Col,
+  Checkbox,
+  Button,
+  AutoComplete,
+} from 'antd';
+import { useParams } from "react-router-dom";
 import "antd/dist/antd.css";
 import "./../index.css";
 
 //wireframe 15
 function PassApp2(){
+	let params = useParams();
+	let id = params.id;
+	
 	return(<div></div>);
 }
 
