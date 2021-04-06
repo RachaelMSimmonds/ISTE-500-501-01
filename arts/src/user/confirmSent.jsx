861a3288a33f697c0f8912b64e4012c9f1328adb
nathankaufman <natecat4444@gmail.com>
2021-04-06 09:27:26 -0400

    More fixes

--- arts/src/user/confirmSent.jsx
+++ arts/src/user/confirmSent.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Button } from 'antd';
+import { useParams } from "react-router-dom";
 import "antd/dist/antd.css";
 import "./../index.css";
 
